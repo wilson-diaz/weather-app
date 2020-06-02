73c1b2e0fe7f6995fba385bf1aad9db07828a964
Wilson Diaz <wdfiguereo@gmail.com>
2020-06-01 23:01:07 -0400

    create city and forecast interfaces. add preliminary html/css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { WeatherComponent } from './weather/weather.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherComponent
   ],
   imports: [
     BrowserModule
