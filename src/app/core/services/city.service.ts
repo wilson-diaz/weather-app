352be132066f9cb41058021bc8732d1348340dfd
Wilson Diaz <wdfiguereo@gmail.com>
2020-06-01 23:07:22 -0400

    add mock city data

--- src/app/core/services/city.service.ts
+++ src/app/core/services/city.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
 
+import { City } from '../interfaces/city';
+import { CITIES } from '../../mocks/cities.mock';
+
 @Injectable({
   providedIn: 'root'
 })
 export class CityService {
 
   constructor() { }
+
+  getCities(): Array<City> {
+    return CITIES;
+  }
 }
